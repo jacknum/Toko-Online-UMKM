682e7b7b8aaf918226104c903ec9821e3edfd8a3
jacknum <syahrulzaki0706@gmail.com>
2025-11-21 18:30:06 +0700

    Fix - railway - 8

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/seller.css',
+                'resources/css/buyer.css',
+                'resources/css/landing.css',
+                'resources/css/auth.css',
+                'resources/js/bootstrap.js',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
