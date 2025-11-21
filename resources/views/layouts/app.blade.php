d5002ba42a0273f0164ecb864d47347099ce171d
jacknum <syahrulzaki0706@gmail.com>
2025-11-21 18:18:54 +0700

    Fix - railway - 7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title', 'Dashboard Toko Online')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/seller.css'])
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
